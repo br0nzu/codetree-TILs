927668e69c9ce1789f529f60b68538566ff717ed
br0nzu <108587715+br0nzu@users.noreply.github.com>
2024-02-27 18:24:44 +0900

    Title: 코로나 메뉴얼, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240227/코로나 메뉴얼/covid-manual.c
+++ 240227/코로나 메뉴얼/covid-manual.c
@@ -3,9 +3,7 @@
 int main() {
     char dis1, dis2, dis3;
     int temp1, temp2, temp3;
-    scanf("%c %d", &dis1, &temp1);
-    scanf("%c %d", &dis2, &temp2);
-    scanf("%c %d", &dis3, &temp3);
+    scanf("%c %d %c %d %c %d", &dis1, &temp1, &dis2, &temp2, &dis3, &temp3);
 
     if((dis1 == 'Y' && temp1 >= 37) && (dis2 == 'Y' && temp2 >= 37)) {
         printf("E");
