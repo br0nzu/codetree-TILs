359da4f2021d588c26f4333f70c3c7c11f233849
br0nzu <108587715+br0nzu@users.noreply.github.com>
2024-02-27 11:57:35 +0900

    Title: 장학금, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240227/장학금/scholarship.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int mid, fin;
+    scanf("%d %d", &mid, &fin);
+
+    if(mid >= 90) {
+        if(fin >= 95) {
+            printf("100000");
+        }
+        else if (fin >= 90) {
+            printf("50000");
+        }
+    }
+    else {
+        printf("0");
+    }
+
+    return 0;
+}
