43efb5349840383c8ea1bc28cc3ca943ebe12233
br0nzu <108587715+br0nzu@users.noreply.github.com>
2024-02-27 12:36:18 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240227/윤년인가/is-leap-year.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int y;
+    scanf("%d", &y);
+
+    if(y % 4 == 0 ||!(y % 100 == 0 && y % 4 == 0)) {
+        printf("true");
+    }
+    else {
+        printf("false");
+    }
+
+    return 0;
+}
