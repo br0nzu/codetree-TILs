bd0d92a0481fc251305499574e0fb5c2acd763f2
br0nzu <108587715+br0nzu@users.noreply.github.com>
2024-02-29 12:28:31 +0900

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240229/a/b 출력/a-divide-b.c
+++ 240229/a/b 출력/a-divide-b.c
@@ -3,15 +3,14 @@
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
-    int c = (a * 10) % b;
-    int d = (a * 10) / b; 
 
     printf("0.");
 
     for(int i = 1; i <= 20; i++) {
-        printf("%d", d);
-        c = (c * 10) % b;
-        d = (c * 10) % b;
+        a *= 10;
+        printf("%d", a / b);
+
+        a %= b;
     }
 
     return 0;
