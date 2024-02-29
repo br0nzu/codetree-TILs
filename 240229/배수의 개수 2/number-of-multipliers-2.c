dbe65c0f81befcce2237fc4d51409cebf423e5e3
br0nzu <108587715+br0nzu@users.noreply.github.com>
2024-02-29 14:17:54 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240229/배수의 개수 2/number-of-multipliers-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int cnt = 0;
+    
+    for(int i = 0; i < 10; i++) {
+        int n;
+        scanf("%d", &n);
+
+        if(n % 2 != 0) {
+            cnt++;
+        }
+    }
+
+    printf("%d", cnt);
+
+    return 0;
+}
