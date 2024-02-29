6e778298984defe547547e552e4e10ed29852ba5
br0nzu <108587715+br0nzu@users.noreply.github.com>
2024-02-29 14:45:22 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240229/윤년 횟수 구하기/number-of-leap-years.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int cnt = 0;
+
+    for(int i = 1; i <= n; i++) {
+        if(i % 4 == 0) {
+            cnt++;
+            if(i % 100 == 0 && i % 400 != 0) {
+                cnt--;
+            }
+        }
+    }
+
+    printf("%d", cnt);
+
+    return 0;
+}
