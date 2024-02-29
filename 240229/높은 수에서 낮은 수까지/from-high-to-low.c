35587fe764e60d83742675397a7d15a20b0294ad
br0nzu <108587715+br0nzu@users.noreply.github.com>
2024-02-29 12:44:12 +0900

    Title: 높은 수에서 낮은 수까지, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240229/높은 수에서 낮은 수까지/from-high-to-low.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if(a >= b) {
+        for(a; a >= b; a--) {
+            printf("%d ", a);
+        }
+    }
+    else {
+        for(b; b >=a; b--) {
+            printf("%d ", b);
+        }
+    }
+
+    return 0;
+}
