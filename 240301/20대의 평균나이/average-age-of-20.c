9150cf28ee2159c52e3183aa4b487c44c64c650d
br0nzu <108587715+br0nzu@users.noreply.github.com>
2024-03-01 21:45:29 +0900

    Title: 20대의 평균나이, Time: 1084ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240301/20대의 평균나이/average-age-of-20.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    int sum = 0, cnt = 0;
+    
+    while(1) {
+        scanf("%d", &age);
+        
+        if(age < 20 && age >= 30) {
+            double average = (double)sum / cnt;
+            printf("%.2lf", average);
+            break;
+        }
+        
+        sum += age;
+        cnt++;
+
+    }
+    return 0;
+}
