035c2cafb103c7923525120c23498b3e50a23c1a
br0nzu <108587715+br0nzu@users.noreply.github.com>
2024-03-02 13:52:55 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240302/합성수 판별기/factorization-discriminator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int cnt = 0;
+
+    for(int i = 1; i <= n; i++) {
+        if(n % i == 0) {
+            cnt++; 
+        }
+    }
+
+    if(cnt == 2) {
+        printf("N");
+    }
+    else {
+        printf("C");
+    }
+
+    return 0;
+}
