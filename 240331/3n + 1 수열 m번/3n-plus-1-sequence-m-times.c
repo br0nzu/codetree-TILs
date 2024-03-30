191ce7e23d6e49b36089ef5dd8c04730b530d831
br0nzu <108587715+br0nzu@users.noreply.github.com>
2024-03-31 04:53:41 +0900

    Title: 3n + 1 수열 m번, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240331/3n + 1 수열 m번/3n-plus-1-sequence-m-times.c
+++ 240331/3n + 1 수열 m번/3n-plus-1-sequence-m-times.c
@@ -11,13 +11,13 @@ int main() {
         int cnt = 0;
 
         while(n != 1) {
-            cnt++;
             if(n % 2 == 0) {
                 n /= 2;
             }
             else {
                 n = n * 3 + 1;
             }
+            cnt++;
         }
 
         printf("%d\n", cnt);
